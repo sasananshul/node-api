4bd2e4d4d394cb8174373375f5b13bb61fb87c0f
sasananshul <sasananshul@outlook.com>
2025-05-16 10:55:51 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/greet', (req, res) => {
+    res.json('Hello World!');
+});
+
 router.get('/:name', (req, res) => {
     const { name } = req.params;
     res.json({ name });
-});
-
-router.get('/greet', (req, res) => {
-    res.json('Hello World!');
 });
 
 module.exports = router;
